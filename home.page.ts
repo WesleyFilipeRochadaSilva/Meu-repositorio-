d021750e3c1fc0f1d10b6cd34c238ca9f5e4466a
WesleyFilipeRochadaSilva <117742910+WesleyFilipeRochadaSilva@users.noreply.github.com>
2022-12-04 23:34:03 -0300

    Add files via upload

---
+++ home.page.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  weight: number = 0
+  height: number = 0
+  imc: number = 0
+
+  constructor(private toastCtrl: ToastController) {}
+
+  onCalculate() {
+    if (this.height <= 0 || this.weight <= 0) {
+      return
+    }
+    
+    this.imc = this.weight / (this.height * this.height)
+    this.showIMC()
+  }
+
+  async showIMC() {
+      if (this.imc < 18.5){
+        const toast = await this.toastCtrl.create({
+        message: `IMC = ${this.imc.toFixed(2)} - MAGREZA`,
+        duration: 3000,
+        color: 'secondary'
+        
+    })
+    toast.present()
+  }
+      if (this.imc >18.5 &&  this.imc < 24.9){
+        const toast = await this.toastCtrl.create({
+          message: `IMC = ${this.imc.toFixed(2)} - NORMAL`,
+          duration: 3000,
+          color: 'secondary'
+          
+      })
+      toast.present()
+
+      }
+       if ((this.imc >25.0) &&  (this.imc < 29.9)){
+          const toast = await this.toastCtrl.create({
+             message: `IMC = ${this.imc.toFixed(2)} - SOBREPESO`,
+             duration: 3000,
+             color: 'secondary'
+        
+        })
+        toast.present()
+     }
+         if (this.imc >30.0 &&  this.imc < 39.9){
+             const toast = await this.toastCtrl.create({
+                message: `IMC = ${this.imc.toFixed(2)} - OBESIDADE`,
+                duration: 3000,
+                color: 'secondary'
+    
+         })
+        toast.present()
+      }
+         if (this.imc > 40.0){
+              const toast = await this.toastCtrl.create({
+              message: `IMC = ${this.imc.toFixed(2)} - OBESIDADE GRAVE`,
+              duration: 3000,
+               color: 'secondary'
+
+         })
+        toast.present()
+      }
+
+  }
+}
